8d658f09ccb96c36ec8a4bc3738d909eeb032d6e
romisfrag <romisfrag@gmail.com>
2016-02-01 22:41:18 +0100

    Ajout en cour de test de la fonction d'évaluation et test de commit

--- lambda_calcul.ml
+++ lambda_calcul.ml
@@ -25,7 +25,6 @@ let alpha_equiv terme_un terme_deux =
 (lambda_term_to_string terme_un) = (lambda_term_to_string terme_deux)
 
 
-
 let reduction t = 
 match t with
 | FreeVar v -> FreeVar v
@@ -33,3 +32,25 @@ match t with
 | Abs x -> Abs(x)
 | Appl(Abs(x),y) -> substitution x 0 y
 | Appl(x,y) -> failwith "erreur"
+
+
+let rec evaluation t = 
+match t with 
+| FreeVar v -> FreeVar v 
+| BoundVar v -> BoundVar v 
+| Abs x -> Abs(x)
+| Appl(Abs(x),y) -> evaluation(reduction t)
+| Appl(x,y) -> Appl(evaluation x, evaluation y)
+
+
+let x = Appl(Abs(Appl(BoundVar 0,BoundVar 0)),Abs(BoundVar 0))
+let y = Appl(Abs(Appl(BoundVar 0, FreeVar "y")),FreeVar "a")
+let w = Appl(Appl(Abs(Abs(BoundVar 0)),FreeVar "x"),Abs(FreeVar "u"))
+let z = Appl(Abs(Appl(BoundVar 0,BoundVar 0)),Abs(FreeVar "u"))
+
+
+
+let () = Printf.printf "%s \n" (lambda_term_to_string (reduction (reduction x)))
+let () = Printf.printf "%s \n" (lambda_term_to_string (x))
+let () = Printf.printf "%s \n" (lambda_term_to_string (evaluation x))
+let () = Printf.printf "%s \n" (lambda_term_to_string (evaluation z))
