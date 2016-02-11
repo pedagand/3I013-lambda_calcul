48d2a3555e195175d17417a9800e0e8208f19fdd
romisfrag <romisfrag@gmail.com>
2016-02-11 19:27:57 +0100

    reduction_forte marche pour les successeurs mais pas encore pour l'addition

--- testUnit.ml
+++
@@ -1,6 +0,0 @@
-open OUnit
-
-let test = 
-"suite">:::
-["test 1">:: test1;
- "test 2">:: test2]
--- testUnit/lambda_calcul.ml
+++ testUnit/lambda_calcul.ml
@@ -3,7 +3,6 @@ type lambda_term =
   | BoundVar of int 
   | Abs of lambda_term
   | Appl of (lambda_term * lambda_term)
-(* rajouter constructeur des vrais ect... *)
 
 
 let rec lambda_term_to_string t = 
@@ -54,7 +53,7 @@ match t with
 | Appl(x,y) -> (Appl(reduction_forte x, reduction_forte y)) *)
 
 
-(* let rec reduction_forte t = 
+let rec reduction_forte t = 
 match t with
 | FreeVar v -> FreeVar v 
 | BoundVar v -> BoundVar v
@@ -62,26 +61,7 @@ match t with
 | Appl(BoundVar x,y) -> Appl(BoundVar x, reduction_forte y)
 | Appl(FreeVar x,y) -> Appl(FreeVar x, reduction_forte y)
 | Appl(Abs(x),y) -> reduction_forte(substitution x 0 (reduction_forte y))
-| Appl(x,y) -> reduction_forte(Appl(reduction_forte x ,reduction_forte y)) *)
-
-let rec relie_libre i bv t =
-match t with 
-| BoundVar v -> BoundVar v
-| FreeVar v -> if v = string_of_int i then BoundVar bv else FreeVar v
-| Abs(x) -> Abs(relie_libre i (bv + 1) x)
-| Appl(x,y) -> Appl(relie_libre i bv x,relie_libre i bv y)
-
-
-let rec reduction_forte t i = 
-match t with 
-| FreeVar v -> FreeVar v
-| BoundVar v -> BoundVar v
-| Abs x -> Abs(relie_libre i 0 (reduction_forte (substitution x 0 (FreeVar (string_of_int i))) (i+1)))
-| Appl(FreeVar x,y) -> Appl(FreeVar x, reduction_forte y i)
-| Appl(Abs(x),y) -> reduction_forte (substitution x 0 y) i
-| Appl(x,y) -> reduction_forte (Appl((reduction_forte x i),y)) i
-
-(* | Appl(Abs(x),Appl(y,z)) -> reduction_forte (Appl(Abs(x),(reduction_forte(Appl(y,z)) i))) i *)
+| Appl(x,y) -> reduction_forte(Appl(reduction_forte x ,reduction_forte y)) 
 	       
 			 (* Les booléens *)
 
@@ -95,7 +75,7 @@ let test2 = Appl(Appl(lambda_if_else,lambda_false),FreeVar "y")
 let test = Appl(Appl(Appl(lambda_if_else,lambda_false),FreeVar "y"),FreeVar "x")
 (*let test3 = Appl(lambda_if_else, *)
 
-(* let () = Printf.printf "%s \n" (lambda_term_to_string lambda_true)
+let () = Printf.printf "%s \n" (lambda_term_to_string lambda_true)
 let () = Printf.printf "%s \n" (lambda_term_to_string lambda_false)
 let () = Printf.printf "%s \n" (lambda_term_to_string lambda_if_else)
 let () = Printf.printf "%s \n" (lambda_term_to_string test)
@@ -105,7 +85,7 @@ let () = Printf.printf "%s \n" (lambda_term_to_string (evaluation test))
 
 let () = Printf.printf "\n"
 let arguments3 = Appl(Appl(Abs(BoundVar 0),Abs(BoundVar 0)),FreeVar "y")
-let () = Printf.printf "%s \n" (lambda_term_to_string (evaluation arguments3)) *)
+let () = Printf.printf "%s \n" (lambda_term_to_string (evaluation arguments3))
 
 
 
@@ -137,24 +117,16 @@ let rec lambda_term_to_int t =
 
 (* Défintions des termes *)
 
-(* let plus = Abs(Abs(Abs(Abs(Appl(Appl(Appl(Appl (BoundVar 3, BoundVar 1),BoundVar 2),BoundVar 1),BoundVar 0))))) *)
+let plus = Abs(Abs(Abs(Abs(Appl(Appl(Appl(Appl (BoundVar 3, BoundVar 1),BoundVar 2),BoundVar 1),BoundVar 0)))))
 let plus = Abs(Abs(Abs(Abs(Appl(Appl(BoundVar 3,BoundVar 1),Appl(Appl(BoundVar 2,BoundVar 1),BoundVar 0))))))
-let plustest = Appl(Appl(plus,(int_to_lambda_term 1)),(int_to_lambda_term 0))
+let plustest = Appl(Appl(plus,(int_to_lambda_term 2)),(int_to_lambda_term 2))
 let succ = Abs(Abs(Abs(Appl(BoundVar 1,Appl(Appl(BoundVar 2,BoundVar 1),BoundVar 0)))))
-
-
-
-let testsucc = Appl(succ,(int_to_lambda_term 4))
-
-let () = Printf.printf "%s \n" (lambda_term_to_string(reduction_forte testsucc 0))
-let () = Printf.printf "%d \n" (lambda_term_to_int(reduction_forte testsucc 0))		
-(*
-let () = Printf.printf "%s \n" (lambda_term_to_string(plustest))
-let () = Printf.printf "%s \n" (lambda_term_to_string(reduction_forte plustest 0)) *)  
-(*
+let testsucc = Appl(succ,(int_to_lambda_term 2))
+		
+(*let () = Printf.printf "%s \n" (lambda_term_to_string(evaluation plustest))
+let () = Printf.printf "%s \n" (lambda_term_to_string(reduction_forte(reduction_forte(reduction_forte plustest)))) *)
 let () = Printf.printf "%s \n" (lambda_term_to_string(reduction_forte testsucc))
 let () = Printf.printf "%d \n" (lambda_term_to_int(reduction_forte testsucc))
- *)
 (*let () = Printf.printf "%s \n" (lambda_term_to_string(reduction_forte plustest)) *) 
 						 
 (*let () = Printf.printf "%s \n" (lambda_term_to_string(plustest))
@@ -174,18 +146,3 @@ let () = Printf.printf "%d \n" (lambda_term_to_int(int_to_lambda_term 0)) *)
 
 
 						 
-
-
-(*test pour la fonction relie libre *)
-
-(* let x = Abs(FreeVar "0")
-let () = Printf.printf "%s \n" (lambda_term_to_string(x))
-let () = Printf.printf "%s \n" (lambda_term_to_string(relie_libre 0 x))
-let y = Abs(Abs(Appl(FreeVar "1",Appl(FreeVar "0",BoundVar 0)))) *)
-
-(*tests pour la fonction reduction_forte *)
-
-
-
-
-
