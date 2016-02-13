039520071fe15e337c2750864b2fc8e650dd9152
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2016-02-14 00:38:00 +0100

    Integrate tests into booleans

--- test.ml
+++ test.ml
@@ -1,7 +1,8 @@
 open OUnit2
 
 let suite =
-  test_list [ "Parser tests" >::: ParserT.tests ]
+  test_list [ "Parser tests" >::: ParserT.tests
+            ; "Boolean test" >::: BooleanT.tests ]
 
 let () =
   run_test_tt_main suite
