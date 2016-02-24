4222b1ed97d3c2e177140a89226e3c1d3498e944
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2016-02-24 09:44:28 +0100

    Do not push build files on the git repos.

--- _build/boolean.ml
+++
@@ -1,9 +0,0 @@
-open Lambda
-
-(** * Booleans *)
-
-let ctrue = read "(lambda (tru fls) tru)"
-let cfalse = read "(lambda (tru fls) fls)"
-let cifthenelse = read "(lambda (b ifTru ifFls) (b ifTru ifFls))"
-
-let () = Printf.printf "%s \n" (lambda_term_to_string(ctrue))
--- _build/test.ml
+++
@@ -1,9 +0,0 @@
-open OUnit2
-
-let suite =
-  test_list [ "Parser tests" >::: ParserT.tests
-            ; "Boolean test" >::: BooleanT.tests 
-            ; "Nat test" >::: NatT.tests ]
-
-let () =
-  run_test_tt_main suite
