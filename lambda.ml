bcc2a6cebe116bd681d014eaf00e7904915db88f
romisfrag <romisfrag@gmail.com>
2016-02-24 20:21:14 +0100

    Modification de lambda.ml avec les fonctions de check et de synth qui semblent marcher

--- lambda.ml
+++ lambda.ml
@@ -1,171 +1,73 @@
 open Sexplib
 
-(*
-  To load in the OCaml toplevel:
-  
-  #use "topfind";;
-  #require "sexplib";;
- 
-*)
-type lambda_term =
-  | FreeVar of string 
-  | BoundVar of int 
-  | Abs of lambda_term
-  | Appl of (lambda_term * lambda_term)
-  | True | False | IfThenElse of lambda_term * lambda_term * lambda_term
 
-(* TODO: remember the name of the abstractions, for pretty-printing *)
-(* TODO: rajouter constructeur des vrais ect... *)
+type typ = 
+| Bool
+| Nat 
+| Fleche of typ * typ
 
-(** * A simple parser *)
+type iN = 
+  | Abs of string * iN
+  | INv of ex
+and ex = 
+  | Var of string
+  | Appl of ex * iN
 
-let rec parse env t 
-    = let rec lookup_var env n v
-        = match env with
-        | [] -> FreeVar v
-        | w :: env when v = w -> BoundVar n
-        | _ :: env -> lookup_var env (n+1) v 
-      in
-      match t with
-      | Sexp.List [Sexp.Atom "lambda"; Sexp.Atom var; body] -> 
-         Abs (parse (var :: env) body)
-      | Sexp.List [Sexp.Atom "lambda"; Sexp.List vars; body] -> 
-         let vars = List.map (function 
-           | Sexp.Atom v -> v
-           | _ -> failwith "Parser: invalid variable") vars 
-         in
-         List.fold_right 
-           (fun var b -> Abs b)
-           vars
-           (parse (List.append (List.rev vars) env) body)
-      | Sexp.Atom v -> lookup_var env 0 v
-      | Sexp.List (f :: args) -> 
-         List.fold_left 
-           (fun x y -> Appl (x, y))
-           (parse env f) 
-           (List.map (parse env) args)
-      | _ -> failwith "Parser: ill-formed input."
+let rec ex_to_string t = 
+match t with
+| Var x -> x 
+| Appl(x,y) -> ex_to_string x ^ " " ^ iN_to_string y
+and iN_to_string t = 
+match t with 
+| Abs (x,y) -> "[]." ^ x ^ iN_to_string y
+| INv x -> ex_to_string x
 
-let read t = parse [] (Sexp.of_string t)
+let x = Abs("f",Abs("a",INv(Appl(Var "f",INv(Var "a")))))
 
-(** * A simple printer *)
+let () = Printf.printf "%s \n" (iN_to_string x)
 
-(* TODO: print S-expression instead. *)
-let rec lambda_term_to_string t = 
-  match t with
-  | FreeVar v -> v
-  | BoundVar v -> string_of_int v        
-  | Abs x -> "[]." ^ lambda_term_to_string x 
-  | Appl (x,y) -> "(" ^ lambda_term_to_string x ^ " " ^ lambda_term_to_string y ^ ")"
-  | True -> "True"
-  | False -> "False"
-  | IfThenElse (x,y,z) -> "if " ^ lambda_term_to_string x ^ " then " ^ lambda_term_to_string y ^ " else " ^ lambda_term_to_string z 
+(* Fonctions préliminaires pour le type checking *)
+let rec retourne_type contexte var = 
+match contexte with 
+| [] -> failwith "La variable n'est pas dans le contexte"
+| (x,y)::z -> if x = var then y else retourne_type z var
 
-let () = Printf.printf "%s \n" (lambda_term_to_string(IfThenElse(True,Abs(BoundVar 0),Abs(BoundVar 1))))
+let is_a_Fleche f = 
+  match f with 
+  | Nat -> false 
+  | Bool -> false 
+  | Fleche(x,y) -> true 
 
-(* QUESTION: Ici je ne vois pas comment représenter le True parceque si on considère que c'est un lambda_term a part entiere alors on l'affiche simplement comme ci dessus mais sinon il faut le définir autrement *)
-(* REPONSE PROBABLE: je pense que cela permettrais d'alleger la syntaxe pour construire un lambda_term de type true puisque toujours le meme mais au tel cas il faut bien modifier l'ensemble des fonctions pour qu'elles evaluent le true comme une Abs(Abs(BoundVar 1)) *)
+(* C'est moche mais je ne sais pas faire autrement  *)
+let typ_gauche_Fleche t =
+match t with 
+| Nat -> failwith "Ce n'est pas une fleche" 
+| Bool -> failwith "Ce n'est pas une fleche"
+| Fleche(x,y) -> x
+
+let typ_droit_Fleche t =
+match t with 
+| Nat -> failwith "Ce n'est pas une fleche" 
+| Bool -> failwith "Ce n'est pas une fleche"
+| Fleche(x,y) -> y
 
 
-(** * Reduction *)
-
-let rec substitution t var tsub 
-    = match t with 
-    | FreeVar v -> FreeVar v 
-    | BoundVar v when v = var -> tsub
-    | BoundVar v -> BoundVar v
-    | Abs x -> Abs(substitution x (var+1) tsub)
-    | Appl (x,y) -> Appl(substitution x var tsub,substitution y var tsub)
-    | True -> Abs(tsub)
-    | False -> False
-    | IfThenElse (x,y,z) -> Abs(Abs(Appl(Appl(tsub,BoundVar 1),BoundVar 0)))
+let x = Fleche(Bool,Nat)
+let () = Printf.printf "Fleche %b \n" (is_a_Fleche Bool)
 
 
+(* i:iN et t:typ e:ex  *)
+let rec check contexte i t = 
+match i with
+| INv(Appl(x,y)) -> check contexte y (typ_gauche_Fleche(synth contexte x)) 
+| INv(Var x) -> if t = (synth contexte (Var x)) then true else false
+| Abs(x,y) -> if (is_a_Fleche t) && check ((x,typ_gauche_Fleche t)::contexte) y (typ_droit_Fleche t) then true else false
+and synth contexte e = 
+match e with 
+| Var x -> retourne_type contexte x 
+| Appl(x,y) -> failwith "Cela ne devrait pas arriver"
 
-(* XXX: Unnecessarily complex: it is enough to compare the raw terms *)
-let alpha_equiv terme1 terme2 = 
-  lambda_term_to_string terme1 = lambda_term_to_string terme2
-
-let rec reduction t 
-    = match t with
-    | FreeVar v -> FreeVar v
-    | BoundVar v -> BoundVar v
-    | Abs x -> Abs(x)
-    | Appl(Abs(x),y) -> substitution x 0 y
-    | Appl(x,y) -> failwith "erreur reduction"
-    | True -> True
-    | False -> False
-    | IfThenElse(x,y,z) -> IfThenElse(x,y,z)
-
-
-let rec evaluation t 
-    = match t with 
-    | FreeVar v -> FreeVar v 
-    | BoundVar v -> BoundVar v 
-    | Abs x -> Abs x
-    | Appl(Abs(x),y) -> evaluation(reduction t)
-    | Appl(BoundVar x,y) -> Appl(BoundVar x,y)
-    | Appl(FreeVar x,y) -> Appl(FreeVar x,y)
-    | Appl(x,y) -> evaluation(Appl(evaluation x, y))
-    | True -> True
-    | False -> False
-    | IfThenElse (x,y,z) when x = True -> y
-    | IfThenElse (x,y,z) when x = False -> z
-    | IfThenElse (x,y,z) -> reduction((IfThenElse ((reduction x), y, z)))
-
-let () = Printf.printf "%s \n" (lambda_term_to_string(evaluation(IfThenElse(True,BoundVar 5,BoundVar 2))))
-
-
-let rec relie_libre i bv t 
-    = match t with 
-    | BoundVar v -> BoundVar v
-    | FreeVar v when v = string_of_int i -> BoundVar bv
-    | FreeVar v -> FreeVar v
-    | Abs(x) -> Abs(relie_libre i (bv + 1) x)
-    | Appl(x,y) -> Appl(relie_libre i bv x,relie_libre i bv y)
-    | True -> True
-    | False -> False
-    | IfThenElse(x,y,z) -> IfThenElse(x,y,z)		       
-
-let rec reduction_forte t i 
-    = match t with 
-    | FreeVar v -> FreeVar v
-    | BoundVar v -> BoundVar v
-    | Abs x -> Abs(relie_libre i 0 (reduction_forte (substitution x 0 (FreeVar (string_of_int i))) (i+1)))
-    | Appl(FreeVar x,y) -> Appl(FreeVar x, reduction_forte y i)
-    | Appl(Abs(x),y) -> reduction_forte(substitution x 0 y) i
-    | Appl(x,y) -> reduction_forte (Appl((reduction x ),y)) i
-    | True -> True
-    | False -> False
-    | IfThenElse (x,y,z) when x = True -> y
-    | IfThenElse (x,y,z) when x = False -> z
-    | IfThenElse (x,y,z) -> reduction((IfThenElse ((reduction x), y, z)))
-
-
-(* | Appl(Abs(x),Appl(y,z)) -> Appl(Abs(x),(Appl(y,z))) *)
-(* | Appl(Abs(x),Appl(y,z)) -> reduction_forte (Appl(Abs(x),(reduction_forte(Appl(y,z)) i))) i *)
-	       
-
-
-
-
-
-
-
-
-						 
-
-
-(*test pour la fonction relie libre *)
-
-(* let x = Abs(FreeVar "0")
-let () = Printf.printf "%s \n" (lambda_term_to_string(x))
-let () = Printf.printf "%s \n" (lambda_term_to_string(relie_libre 0 x))
-let y = Abs(Abs(Appl(FreeVar "1",Appl(FreeVar "0",BoundVar 0)))) *)
-
-(*tests pour la fonction reduction_forte *)
-
-
-
-
+let x = Abs("f",Abs("g",INv(Appl(Var "f",INv(Var "g")))))
+let t = Fleche(Fleche(Bool,Bool),Fleche(Bool,Bool))
+let () = Printf.printf "resultat type check %b \n" (check [] x t)
 
