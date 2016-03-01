2bc79c11742ca63df0d23f644f0877a814d1f86b
romisfrag <romisfrag@gmail.com>
2016-03-01 22:22:15 +0100

    Ajout finit de la substitution et reduction ainsi que la modification de l'algo de typechecking pour des termes avec BVar et FVar

--- lambda.ml
+++ lambda.ml
@@ -33,7 +33,7 @@ match t with
 | Ann(x,y) -> inTm_to_string x ^ ":" ^ typ_to_string y
 and inTm_to_string t = 
 match t with 
-| Abs (x,y) -> "([]." ^ x  ^ inTm_to_string y ^ ")"
+| Abs (x,y) -> "([]" ^ x  ^ "." ^ inTm_to_string y ^ ")"
 | Inv x -> exTm_to_string x
 
 
@@ -49,7 +49,7 @@ let rec substitution_inTm t tsub var =
 and substitution_exTm  t tsub var = 
   match t with 
   | FVar x -> FVar x
-  | BVar x -> if x = (var - 1) then tsub else BVar x
+  | BVar x -> if x = var then tsub else BVar x
   | Appl(x,y) -> Appl((substitution_exTm x tsub var),(substitution_inTm y tsub var))
   | Ann(x,y) -> Ann((substitution_inTm x tsub var),y)
 
@@ -62,7 +62,7 @@ and reduction_exTm t =
   match t with 
     | FVar x -> FVar x
     | BVar x -> BVar x 
-    | Appl(Ann(Abs(s,x),Fleche(ty,y)),Inv z) -> Ann((substitution_inTm (Abs(s,x)) z 0),y)
+    | Appl(Ann(Abs(s,x),Fleche(ty,y)),Inv z) -> Ann((substitution_inTm x z 0),y)
     | Appl(x,y) -> Appl(x,y)
     | Ann(x,y) -> Ann(x,y)
 
@@ -71,35 +71,37 @@ let() = Printf.printf "%s \n" (inTm_to_string(substitution_inTm x (FVar "w") 0))
 let() = Printf.printf "%s \n\n" (inTm_to_string(substitution_inTm x (Ann(Abs("y",Inv(BVar 0)),Fleche(Bool,Bool))) 0))
 let () = Printf.printf "%s \n" (exTm_to_string y)  
 let () = Printf.printf "%s \n" (exTm_to_string(reduction_exTm y))  
-(*
+
 (* i:inTm et t:typ e:exTm  *)
-let rec check contexte inT ty 
+(* ici le compt doit etre supérieur a toutes les variables liées déja présentes *)
+let rec check contexte inT ty compt
     = match inT with
     | Abs(x, b) -> 
        begin
          match ty with
          | Fleche(s, t) -> 
             (* XXX: open the de Bruijn binder *)
-            check ((x, s) :: contexte) b t
+            check (((string_of_int compt), s) :: contexte) (substitution_inTm b (FVar(string_of_int compt)) 0) t (compt + 1)
          | _ -> failwith "Abstraction forced into a non-functional type"
        end
     | Inv(t) -> 
-       let tyT = synth contexte t in
+       let tyT = synth contexte t compt in
        tyT = ty
-and synth contexte exT 
+and synth contexte exT compt
     = match exT with
     | Ann(tm, ty) ->
-       if check contexte tm ty then
+       if check contexte tm ty compt then
          ty 
        else
          failwith "Wrong annotation"
-    | Var(x) -> List.assoc x contexte
+    | FVar(x) -> List.assoc x contexte
+    | BVar x -> failwith "Bvar is not possible at this moment"
     | Appl(f, s) -> 
-       let fTy = synth contexte f in
+       let fTy = synth contexte f compt in
        begin
          match fTy with
          | Fleche(a, b) -> 
-            if check contexte s a then
+            if check contexte s a compt then
               b
             else 
               failwith "Argument type invalid"
@@ -108,13 +110,14 @@ and synth contexte exT
 
 (* XXX: turn into unit tests *)
 (* [https://en.wikipedia.org/wiki/B,_C,_K,_W_system] *)
-let x = Abs("f",Abs("g",Inv(Appl(Var "f",Inv(Var "g")))))
+let x = Abs("f",Abs("g",Inv(Appl(BVar 1,Inv(BVar 0)))))
 let t = Fleche(Fleche(Bool,Bool),Fleche(Bool,Bool))
-let y = Abs("f",Abs("g",Inv(Appl(Var "g",Inv(Var "f")))))
-let u = Fleche(Fleche(Bool,Bool),Fleche(Bool,Bool))
+let y = Inv(Appl(Ann(Abs("x",Inv(BVar 0)),Fleche(Fleche(Bool,Bool),Fleche(Bool,Bool))),(Abs("y",(Inv(BVar 0))))))
+let u = Fleche(Bool,Bool)
 
 let () = 
-  Printf.printf "resultat type check %b \n" (check [] x t);
-  Printf.printf "resultat type check %b \n" (check [] y u);
+  Printf.printf "truc a checker %s \n" (inTm_to_string x);
+  Printf.printf "resultat type check %b \n" (check [] x t 2);
+  Printf.printf "truc a checker %s \n" (inTm_to_string y);
+  Printf.printf "resultat type check %b \n" (check [] y u 2);
 
- *)
