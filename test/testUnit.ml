38669499e35419224924e28b6a8a87c3c7ea4184
romisfrag <romisfrag@gmail.com>
2016-03-03 16:10:49 +0100

    modification du test en vitesse

--- test/testUnit.ml
+++ test/testUnit.ml
@@ -1,13 +1,14 @@
 open OUnit
 
 
-let test1 test_ctxt = assert_equal Appl(Abs(BoundVar 0),FreeVar v) (Lambda_calcul.evaluation (FreeVar v));;
-let test2 test_ctxt = assert_equal Appl(Abs(BoundVar 0),Abs(BoundVar 0)) (Lambda_calcul.evaluation (Abs(BoundVar 0)));;
+
+
 
 let eval = 
 "eval">:::
 ["test 1">:: test1;
- "test 2">:: test2]
+ "test 2">:: test2;
+ "test 3">:: test3;]
 
 ;;
 let () = 
