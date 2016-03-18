c71403e357d277b0887774d6b74c8da0348b96ba
romisfrag <romisfrag@gmail.com>
2016-03-18 14:12:11 +0100

    type checker en cour, commit pour changer de branch

--- lambda.ml
+++ lambda.ml
@@ -6,32 +6,69 @@ open Sexplib
 type inTm = 
   | Abs of string * inTm
   | Inv of exTm
+  | Pi of inTm * inTm 
+  | Star
 and exTm = 
   | Ann of inTm * inTm 
-  | Star 
-  | Pi of inTm * inTm 
   | BVar of int 
   | FVar of string 
-  | Appl of exTm * inTm 
+  | Appl of exTm * inTm
+
+type value = 
+  | VLam of (value -> value)
+  | VNeutral of neutral 
+  | VStar 
+  | VPi of (value -> value)
+and neutral = 
+  | NFree of string 
+  | NApp of neutral * value 
+and env = Env of value list 
 
 
 (* ici on va crée le parseur lisp avec le pretty printing *)
+
+
+(* fonction de réduction ect *)
+
+
+let vfree name = VNeutral(NFree name)
+
+
 
 
 
 let gensym =
   let c = ref 0 in
   fun () -> incr c; "x" ^ string_of_int !c
-
-(* Premiere remarque, sur le t je ne vois pas comment on va faire pour 
+(*
+(* Premiere remarque, sur le t je ne vois pas comment on va faire pour *)
 let rec check contexte inT ty = 
-(*  match inT with 
+  match inT with 
   | Abs(x,y) -> 
      begin 
      match ty with      
+       (* ici il faut faire la substitution dans y avec la freshVar *)
        | Pi(s,t) -> let freshVar = gensym () in
 		    check ((freshVar,s)::contexte) y t
        | _ -> failwith "a trouver le message d'erreur" 
-     end *)
+     end 
+(* ici il va falloir faire une fonction de réduction avant de pouvoir type checker tout ça réellement *)
+  |  Inv(t) -> 
+      let tyT = synth contexte t in
+      begin 
+	tyT = ty
+      end
+  | Pi(s,t) when check contexte s Star -> let freshVar = gensym () in 
+					  check ((freshVar,s)::contexte) t Star
+  | Star -> 
+     begin 
+      match ty with 
+	| Star -> true 
+	| _ -> failwith "ty must be a Star"
+     end
+  | _ -> failwith "term not typable" 
+and synth contexte exT =
+  match exT with 
+  | BVar *)
 
 
