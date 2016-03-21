d6bb550ecfa0bf3cfb7d46cc1b3a540fae6fdd0a
romisfrag <romisfrag@gmail.com>
2016-03-21 21:10:49 +0100

    nothing

--- boolean.ml
+++ boolean.ml
@@ -6,6 +6,6 @@ let ctrue = read "(lambda (tru fls) tru)"
 let cfalse = read "(lambda (tru fls) fls)"
 let cifthenelse = read "(lambda (b ifTru ifFls) (b ifTru ifFls))"
 
-let () = Printf.printf "%s \n" (lambda_term_to_string(ctrue))
+(* let () = Printf.printf "%s \n" (lambda_term_to_string(ctrue)) *)
 
 
