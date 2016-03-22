6a5c1c9ad251df4e8c7eab81d7f971da3c08489d
romisfrag <romisfrag@gmail.com>
2016-03-22 09:50:34 +0100

    modif test unit

--- test/parserT.ml
+++ test/parserT.ml
@@ -2,11 +2,13 @@ open OUnit2
 open Lambda
 
 let inputs
-    = [("(lambda x x)", Abs (BoundVar 0));
-       ("(lambda x y)", Abs (FreeVar "y"));
-       ("(x y z)", Appl (Appl (FreeVar "x", FreeVar "y"), FreeVar "z"));
-       ("(lambda (x y z) (x (y z)))", Abs (Abs (Abs (Appl (BoundVar 2, Appl (BoundVar 1, BoundVar 0))))));
-       ("(lambda (x y z) (x y z))", Abs (Abs (Abs (Appl (Appl (BoundVar 2, BoundVar 1), BoundVar 0)))))]
+    = [("(lambda x x)", Abs("x",Inv(BVar 0)));
+       ("(lambda x y)", Abs("x",Inv(FVar "y")));
+       ("(x y z)", Inv(Appl(Appl(FVar "x", Inv(FVar "y")), Inv(FVar "z"))));
+       ("(lambda (x y z) (x (y z)))", Abs("x",Abs("y",Abs("z",Inv(Appl(BVar 2, Inv(Appl(BVar 1, Inv(BVar 0)))))))));
+       ("(lambda (x y z) (x y z))", Abs("x",Abs("y",Abs("z",Inv(Appl (Appl (BVar 2, Inv(BVar 1)), Inv(BVar 0)))))));
+       ("((: (lambda x x) *) y)", Inv(Appl(Ann(Abs("x",Inv(BVar 0)),Star),Inv(FVar "y")))) ]
+
 
 let tests
     = List.map (fun (term, res) -> term >:: fun ctxt -> assert_equal (read term) res) inputs
